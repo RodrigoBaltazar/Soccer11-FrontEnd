304b70ea4dc824af393d1524ae3b2ff8d6aee3a4
developer <rodrigobaltazarinf@gmail.com>
2020-09-09 20:48:10 -0300

    tentando arrumar tela goals

--- app/src/app/goals/goals.component.ts
+++ app/src/app/goals/goals.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-goals',
@@ -7,7 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalsComponent implements OnInit {
 
-  constructor() { }
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Card 1', cols: 1, rows: 1 }
+        ];
+      }
+
+      return [
+        { title: 'Card 1', cols: 1, rows: 1 }
+      ];
+    })
+  );
+  
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
   }
