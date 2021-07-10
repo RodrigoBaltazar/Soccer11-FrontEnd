84dafef662f76d4d188f4b5dc41a6189115c92ff
RodrigoBaltazar <rodrigobaltazarinf@gmail.com>
2021-07-10 10:23:29 -0300

    frontend vue

---
+++ app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <MidiaPlayer />
+  </div>
+</template>
+
+<script>
+import MidiaPlayer from './components/MidiaPlayer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MidiaPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/MidiaPlayer.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="midiaplayer">
+        <h3>{{ title }}</h3>
+        <p class="post-author">{{ postAuthor }}</p>
+        <div class="player">
+            {{videoPath}}
+        </div>
+        <p class="votes">{{ votes }}</p>
+        <p class="datetime">{{ datetime }}</p>
+        {{ posts }}
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MidiaPlayer',
+  props: {
+    title: String,
+    postAuthor: String,
+    videoPath: String,
+    votes: Number,
+    datetime: String,
+    isPublished: Boolean
+  },
+  data() {
+    return {
+      posts: null
+    }
+  },
+  created() {
+  // GET request using fetch with set headers
+  const headers = { "Content-Type": "application/json" };
+  fetch("http:127.0.0.1:5000/api/post", { headers })
+    .then(response => response.json())
+    .then(data => (this.posts = data.total));
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+.midiaplayer {
+    border-style: solid;
+}
+</style>
